4d6a8eace51e34eba1a1896a0aa428105876d4a1
Ludgero Bento <lbento525@gmail.com>
2022-11-22 17:42:49 +0000

    converted class component to function compomnent using hooks

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,21 +1,36 @@
-import { Component } from "react";
+//import { Component } from "react";
 import "./search-box.styles.css";
 
-class SearchBox extends Component {
-  render() {
-    const { onChangeHandler, placeholder, className } = this.props;
+const SearchBox = ({ onChangeHandler, placeholder, className }) => {
+  //const { onChangeHandler, placeholder, className } = this.props;
 
-    return (
-      <div>
-        <input
-          className={`search-box ${className}`}
-          type="search"
-          placeholder={placeholder}
-          onChange={onChangeHandler}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <input
+        className={`search-box ${className}`}
+        type="search"
+        placeholder={placeholder}
+        onChange={onChangeHandler}
+      />
+    </div>
+  );
+};
+
+// class SearchBox extends Component {
+//   render() {
+//     const { onChangeHandler, placeholder, className } = this.props;
+
+//     return (
+//       <div>
+//         <input
+//           className={`search-box ${className}`}
+//           type="search"
+//           placeholder={placeholder}
+//           onChange={onChangeHandler}
+//         />
+//       </div>
+//     );
+//   }
+// }
 
 export default SearchBox;
